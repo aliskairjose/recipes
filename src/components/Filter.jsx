ab806b09832bf3c7f3ed69ae7286c19dfc6eb039
aliskairjose <kervingonzalez@gmail.com>
2023-09-20 14:08:09 -0400

    filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,7 @@
 import { createRef, useRef } from "react";
 import PropTypes from "prop-types";
 
-const DIET_COL1 = [
+const DIETS_COL1 = [
   { title: "Vegetarian" },
   { title: "Vegan" },
   { title: "Paleo" },
@@ -9,13 +9,27 @@ const DIET_COL1 = [
   { title: "High-Protein" },
   { title: "Low-Carb" },
 ];
-const DIET_COL2 = [
+const DIETS_COL2 = [
   { title: "Low-Fat" },
   { title: "Low-Sodium" },
   { title: "Low-Sugar" },
   { title: "Alcohol-Free" },
   { title: "Balanced" },
   { title: "Inmunity" },
+];
+
+const ALLERGIES_COL1 = [
+  { title: "Gluten" },
+  { title: "Dairy" },
+  { title: "Eggs" },
+  { title: "Soy" },
+  { title: "Wheat" },
+];
+const ALLERGIES_COL2 = [
+  { title: "Fish" },
+  { title: "Shellfish" },
+  { title: "Tree Nuts" },
+  { title: "Peanuts" },
 ];
 
 export default function Filter({ onSearchAction }) {
@@ -29,7 +43,7 @@ export default function Filter({ onSearchAction }) {
 
   return (
     <form className="" onSubmit={handleOnSubmit}>
-      <div className="mb-4">
+      <div className="mb-4 flex flex-col justify-center items-center">
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="username"
@@ -38,23 +52,26 @@ export default function Filter({ onSearchAction }) {
           ref={inputRef}
           onChange={(event) => onSearchChange(event.target.value)}
         />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline w-1/2"
+          type="button"
+          onClick={() => onSearchAction(term.current)}
+        >
+          Search
+        </button>
       </div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="button"
-        onClick={() => onSearchAction(term.current)}
-      >
-        Buscar
-      </button>
       <div className="my-4">
-        <span className="text-sm text-gray-600">Refina la busqueda por</span>
+        <span className="text-sm text-gray-600 font-light">
+          Refine search by
+        </span>
       </div>
-      <span className="text-sm block font-medium">Dieta</span>
+      
+      <span className="text-sm block font-medium">Diet</span>
       <div className="grid grid-cols-2 mb-4 mt-2">
         <div>
           <ul>
             <li>
-              {DIET_COL1.map((item, index) => (
+              {DIETS_COL1.map((item, index) => (
                 <div className="flex items-center mb-2" key={index}>
                   <input
                     id="default-checkbox"
@@ -73,7 +90,7 @@ export default function Filter({ onSearchAction }) {
         <div>
           <ul>
             <li>
-              {DIET_COL2.map((item, index) => (
+              {DIETS_COL2.map((item, index) => (
                 <div className="flex items-center mb-2" key={index}>
                   <input
                     id="default-checkbox"
@@ -81,7 +98,28 @@ export default function Filter({ onSearchAction }) {
                     value=""
                     className=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label className="ml-2 text-sm">
+                  <label className="ml-2 text-sm">{item.title}</label>
+                </div>
+              ))}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <span className="text-sm font-medium">Allergies</span>
+      <div className="grid grid-cols-2 mb-4 mt-2">
+        <div>
+          <ul>
+            <li>
+              {ALLERGIES_COL1.map((item, index) => (
+                <div className="flex items-center mb-2" key={index}>
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    className=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label className="ml-2 text-sm text-gray-900 dark:text-gray-300">
                     {item.title}
                   </label>
                 </div>
@@ -89,12 +127,23 @@ export default function Filter({ onSearchAction }) {
             </li>
           </ul>
         </div>
-        
-      </div>
-      <span className="text-sm font-medium">Alergia</span>
-      <div className="grid grid-cols-2 mt-2">
-        <div>1</div>
-        <div>2</div>
+        <div>
+          <ul>
+            <li>
+              {ALLERGIES_COL2.map((item, index) => (
+                <div className="flex items-center mb-2" key={index}>
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    className=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label className="ml-2 text-sm">{item.title}</label>
+                </div>
+              ))}
+            </li>
+          </ul>
+        </div>
       </div>
     </form>
   );
