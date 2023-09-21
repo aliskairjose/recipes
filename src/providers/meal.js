9f244c0d859bea933304962a1cacc9f8b26b82b1
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-09-20 23:21:01 -0400

    eliminanod archivos

--- src/providers/meal.js
+++ src/providers/meal.js
@@ -5,16 +5,10 @@ const appKEY = import.meta.env.VITE_API_KEY;
 
 const instance = axios.create({
   baseURL: "https://api.edamam.com/",
-  // params: {
-  //   app_id: appID,
-  //   app_key: appKEY,
-  //   q: "",
-  // },
 });
 const slug = `?app_id=${appID}&app_key=${appKEY}`
 
 export const recipes = async (params) => {
-  console.log(params)
   return await instance
     .get(`search${slug}${params}`)
     .then((response) => response.data)
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import Cocktails  from "../pages/Cocktails";
 import ErrorPage from "../pages/ErrorPage";
 import Meals from "../pages/Meals";
 
@@ -9,20 +7,11 @@ const baseUrl = '/recipes'
 const router = createBrowserRouter([
   {
     path: baseUrl,
-    // element: <Home />
-    element: <Meals />
-  },
-  {
-    path:  `${baseUrl}/cocktails`,
-    element: <Cocktails />
-  },
-  {
-    path: `${baseUrl}/meals`,
     element: <Meals />
   },
   {
     path: "/",
-    element: <Home />,
+    element: <Meals />,
     errorElement: <ErrorPage />,
   },
 ]);
