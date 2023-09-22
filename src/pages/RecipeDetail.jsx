1dee61cb6d62835b6f09e2fa459ee603acaea33d
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-09-22 13:45:09 -0400

    detalle

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,9 +1,26 @@
 import { useSelector } from "react-redux";
 
 export default function RecipeDetail() {
-  const recipe = useSelector(state => state.recipe.recipe)
+  const recipe = useSelector((state) => state.recipe.recipe);
 
   return (
-    <div>{recipe.label}</div>
-  )
+    <div className="grid grid-rows-2 w-8/12 content-center mx-auto">
+      <div className="flex">
+        <div className="p-4">
+          <img
+            className="p-2 h-96 w-auto"
+            src={recipe.image}
+            alt={recipe.label}
+          />
+        </div>
+        <div className="w-1/2 text-center p-6">
+          <h1 className="text-3xl">{recipe.label}</h1>
+        </div>
+      </div>
+      <div className="flex">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
