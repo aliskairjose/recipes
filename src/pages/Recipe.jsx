5181f5e6b686a48d54cd1f0551be7757452f44dc
aliskairjose <kervingonzalez@gmail.com>
2023-09-23 13:03:33 -0400

    scroll

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,15 +1,33 @@
 import { useSelector } from "react-redux";
 import Spinner from "../components/Spinner";
 import RecipeCard from "../components/RecipeCard";
+import { useEffect } from "react";
 
 export default function Recipe() {
   const recipes = useSelector((state) => state.recipe.recipes);
-  const isLoading = useSelector(state => state.loading.isLoading)
+  const isLoading = useSelector((state) => state.loading.isLoading);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isLoading
+    ) {
+      return;
+    }
+    console.log('data')
+    // fetchData();
+  };
 
   return (
     <div className="flex flex-wrap gap-2 pt-6">
       {isLoading}
-       {isLoading ? (
+      {isLoading ? (
         <Spinner />
       ) : (
         recipes.hits?.map((item, index) => (
